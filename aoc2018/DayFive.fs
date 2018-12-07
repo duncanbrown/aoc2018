e84bc10f69d431e43239ac73fbdcce1954c49a02
duncanbrown <duncanphilipbrown@hotmail.com>
2018-12-07 20:11:04 +0000

    five b

--- aoc2018/DayFive.fs
+++ aoc2018/DayFive.fs
@@ -1,6 +1,11 @@
 ﻿module DayFive
 
 open System.IO
+open System
+open Microsoft.FSharp
+open Microsoft.FSharp.Collections
+open FSharp.Collections.ParallelSeq
+open System
 
 let reactivePairs =
     List.zip ['a'..'z'] ['A'..'Z']
@@ -10,14 +15,29 @@ let reactivePairs =
 let reducePolymer polymer =
     let rec loop (p: char[]) =
         let pairs = Seq.pairwise p
-        match Seq.tryFindIndex (fun p -> Set.contains p reactivePairs) pairs with
+        match Seq.tryFindIndex (fun (c, c') -> c <> c' && (c = Char.ToLower c' || c = Char.ToUpper c')) pairs with
         | Some i ->
-            let newP = Array.append p.[0..i-1] p.[i+2..]
+            let span = p.AsSpan()
+            let newP = Array.append ((span.Slice (0, i)).ToArray ()) ((span.Slice (i+2)).ToArray ())
             loop newP
         | None -> p
     polymer |> Array.ofSeq |> loop |> Seq.length
+
+let pruneAndReduce (polymer: string) =
+    let units = ['a'..'z']
+    let pruneUnitAndReduce unit =
+        let pruned = polymer.Replace(string unit, "", StringComparison.InvariantCultureIgnoreCase)
+        reducePolymer pruned    
+
+    units
+        |> PSeq.map pruneUnitAndReduce
+        |> PSeq.min
 
 
 let part1 () =
     let input = File.ReadAllText "c:\\dev\\aoc2018\\input\\5.txt"
     reducePolymer (input.Trim ())
+    
+let part2 () =
+    let input = File.ReadAllText "c:\\dev\\aoc2018\\input\\5.txt"
+    pruneAndReduce (input.Trim ())
--- aoc2018Test/DayFive.fs
+++ aoc2018Test/DayFive.fs
@@ -9,3 +9,10 @@ let ``reducePolymer works`` () =
     let expected = 10
     let actual = reducePolymer input
     Assert.Equal(expected , actual)
+
+[<Fact>]
+let ``pruneAndReducePolymer works`` () =
+    let input = "dabAcCaCBAcCcaDA"
+    let expected = 4
+    let actual = pruneAndReduce input
+    Assert.Equal(expected , actual)
