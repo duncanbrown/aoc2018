b753724e856aeb79723fb10b0cc3622013af5d64
duncanbrown <duncanphilipbrown@hotmail.com>
2018-12-09 12:41:09 +0000

    nine b

--- aoc2018/DayNine.fs
+++ aoc2018/DayNine.fs
@@ -1,52 +1,75 @@
 ﻿module DayNine
 
-let insertAfter x i (xs: 'T list) =
-    List.concat [ xs.[0..i]; [x]; xs.[i+1..] ]
+open System
+open System.Collections.Generic
 
-let replace x i (xs: 'T list) =
-    List.concat [ xs.[0..i-1]; [x]; xs.[i+1..] ]
+let insertAfter x i (xs: List<int>)  =
+    xs.Insert (i+1, x)
+    //xs
+    //let xs' = Array.zeroCreate (xs.Length + 1)
+    //Array.blit xs 0 xs' 0 (i+1)
+    //Array.set xs' (i+1) x
+    //Array.blit xs (i+1) xs' (i+2) (xs.Length - i - 1)
+    //xs'
 
-let removeAt i (xs: 'T list) =
-    xs.[i], List.concat [ xs.[0..i-1]; xs.[i+1..] ]
+let replace x i (xs: 'T[]) =
+    let xs' = Array.copy xs
+    Array.set xs' i x
+    xs'    
+
+let removeAt i (xs: List<int>) =
+    let x = xs.[i]
+    xs.RemoveAt (i)
+    x
+    //let x = xs.[i]
+    //let xs' = Array.zeroCreate (xs.Length - 1)
+    //Array.blit xs 0 xs' 0 i
+    //Array.blit xs (i+1) xs' i (xs.Length - i - 1)
+    //x, xs'
 
 let modulo n m = ((n % m) + m) % m;;
 
-let getIndexToInsertAfter circle currentMarble =
-    let currentMarbleIndex = List.findIndex (fun m -> m = currentMarble) circle
-    let length = List.length circle
+let getIndexToInsertAfter (circle: List<int>) currentMarbleIndex =
+    let length  = circle.Count
     if length = 0 then 0
     else
         (currentMarbleIndex + 1) % length
     
-let getIndexOfMarbleToRemove circle currentMarble =
-    let currentMarbleIndex = List.findIndex (fun m -> m = currentMarble) circle
-    let length = List.length circle
+let getIndexOfMarbleToRemove (circle: List<int>) currentMarbleIndex =
+    let length = circle.Count
     modulo (currentMarbleIndex - 7) length
 
 
 let score players lastMarble =
     let marbles = { 1 .. lastMarble }
-    let initialScores = List.replicate players 0
-    let initialCircle = [0]
-    let folder (scores: int list, circle, currentMarble, playerIndex) marble =
-        let playerIndex' = (playerIndex + 1 ) % List.length scores
+    let initialScores = Array.replicate players 0L
+    let initialCircle = List<int> (lastMarble + 1)
+    initialCircle.Add (0)
+    let folder (scores: int64[], circle: List<int>, currentMarble, currentMarbleIndex, playerIndex) marble =
+        if marble % 1000 = 0
+        then printfn "%i" marble
+        else ()
+        let playerIndex' = (playerIndex + 1 ) % Array.length scores
         if marble % 23 <> 0
         then
-            let indexToAddMarble = getIndexToInsertAfter circle currentMarble
-            let circle' = insertAfter marble indexToAddMarble circle
-            (scores, circle', marble, playerIndex')
+            let indexToAddMarbleAfter = getIndexToInsertAfter circle currentMarbleIndex
+            insertAfter marble indexToAddMarbleAfter circle
+            (scores, circle, marble, indexToAddMarbleAfter+1, playerIndex')
         else
-            let indexOfMarbleToRemove = getIndexOfMarbleToRemove circle currentMarble
-            let removed, circle' = removeAt indexOfMarbleToRemove circle
+            let indexOfMarbleToRemove = getIndexOfMarbleToRemove circle currentMarbleIndex
+            let removed = removeAt indexOfMarbleToRemove circle
             let score = scores.[playerIndex]
-            let score' = score + marble + removed
+            let score' = score + (int64 marble) + (int64 removed)
             let scores' = replace score' playerIndex scores
-            let marble' = circle.[indexOfMarbleToRemove + 1]
-            (scores', circle', marble', playerIndex')
-    let finalScores, _, _, _ =
+            let marble' = circle.[indexOfMarbleToRemove]
+            (scores', circle, marble', indexOfMarbleToRemove, playerIndex')
+    let finalScores, _, _, _, _ =
         marbles
-        |> Seq.fold folder (initialScores, initialCircle, 0, 0)
-    List.max finalScores
+        |> Seq.fold folder (initialScores, initialCircle, 0, 0, 0)
+    Array.max finalScores
 
 let part1 () =
     score 428 70825
+
+let part2 () =
+    score 428 7082500
