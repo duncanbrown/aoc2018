52a7a1b6d85ea7b701f762eb554876a205f9fef5
duncanbrown <duncanphilipbrown@hotmail.com>
2018-12-16 11:56:13 +0000

    fifteen b

--- aoc2018/DayFifteen.fs
+++ aoc2018/DayFifteen.fs
@@ -2,6 +2,7 @@
 
 open System
 open FSharp.Collections.ParallelSeq
+open System.Diagnostics
 
 type UnitPoints = { hitPoints: int; attackPower: int }
 
@@ -63,9 +64,9 @@ let getCell (world: World) (x,y) =
     world.[y].[x]
 
 let print (world: World) =
-    //world
-    //|> Array.iter (fun row ->
-    //    row |> Array.map toString |> charsToString |> printfn "%s" )
+    world
+    |> Array.iter (fun row ->
+        row |> Array.map toString |> charsToString |> printfn "%s" )
     let units = getUnitsAndPositions world
     units
     |> Array.map snd
@@ -121,7 +122,7 @@ let getNextMove world unit pos =
                 |> Seq.map fst
                 |> List.ofSeq
                 |> Choice2Of2
-        let pathsAndNextSteps = paths |> Seq.map (fun p -> p, getSteps p) |> List.ofSeq
+        let pathsAndNextSteps = paths |> PSeq.map (fun p -> p, getSteps p) |> List.ofSeq
         let pathsAlreadyAtEnemies =
             pathsAndNextSteps
             |> List.filter ( fun (_, nextSteps) ->
@@ -195,11 +196,11 @@ let tick unit pos world =
         | _ -> pos, world
     let targetPos =
         getAdjacentEnemies worldAfterMove unit newPos
-        |> List.sortBy (fun (p, c) ->
+        |> List.sortBy (fun ((x,y), c) ->
             let (Unit u) = c
             match u with
-            | Elf d -> d.hitPoints
-            | Goblin d -> d.hitPoints)
+            | Elf d -> d.hitPoints, y, x
+            | Goblin d -> d.hitPoints, y, x)
         |> List.tryHead
         |> Option.map fst
     match targetPos with
@@ -213,42 +214,67 @@ let tick unit pos world =
         updateUnit world t target'    
             
 
+let getElfCount units = 
+    units
+    |> Array.map snd
+    |> Array.filter (function
+        | Elf _ -> true
+        | Goblin _ -> false)
+    |> Array.length
+
+let getGoblinCount units = 
+    units
+    |> Array.map snd
+    |> Array.filter (function
+        | Elf _ -> true
+        | Goblin _ -> false)
+    |> Array.length
+
 let tickWorld world =
-    print world
+    //print world
     //Console.ReadLine() |> ignore
+    let sw = Stopwatch.StartNew()
     let unitsInOrder = getUnitsAndPositions world
+    let elfCount = getElfCount unitsInOrder
     let folder acc (i, (unitPos, unit)) =
         printfn "Unit %i" i
         match getCell acc unitPos with
         | Open   -> acc // Unit has been destroyed
-        | Unit _ -> tick unit unitPos acc
-    unitsInOrder |> Array.mapi (fun i u -> i,u) |> Array.fold folder world
+        | Unit _ -> tick unit unitPos acc            
+    let r = unitsInOrder |> Array.mapi (fun i u -> i,u) |> Array.fold folder world
+    printfn "%A" sw.Elapsed
+    r
 
 let run world =
+    let initialElfCount = world |> getUnitsAndPositions |> getElfCount
     let rec loop w i =
         printfn "Loop %i" i
-        let units = 
-            getUnitsAndPositions w
-            |> Array.map snd
-        let gameOver =
-            units
-            |> Array.distinctBy (fun u -> u.GetType())
-            |> Array.length = 1
-        if gameOver
-        then
-            let hitPointsLeft =
-                units
-                |> Array.sumBy (function
-                    | Elf d -> d.hitPoints
-                    | Goblin d -> d.hitPoints)
-            hitPointsLeft * (i-1), hitPointsLeft * i
+        let units = w |> getUnitsAndPositions
+        let elfCount = getElfCount units
+        if elfCount < initialElfCount then None
         else
-            loop (tickWorld w) (i+1)
+            let gameOver =
+                units
+                |> Array.map snd
+                |> Array.distinctBy (fun u -> u.GetType())
+                |> Array.length = 1
+            if gameOver
+            then
+                let hitPointsLeft =
+                    units
+                    |> Array.map snd
+                    |> Array.sumBy (function
+                        | Elf d -> d.hitPoints
+                        | Goblin d -> d.hitPoints)
+                Some (hitPointsLeft * (i-1), hitPointsLeft * i)
+            else
+                loop (tickWorld w) (i+1)
     loop world 0
 
-let parseWorld lines =
+let parseWorld lines elfHitPower =
+    printfn "Elf power: %i" elfHitPower
     let g = Unit (Goblin { hitPoints = 200; attackPower = 3; })
-    let e = Unit (Elf { hitPoints = 200; attackPower = 3; })
+    let e = Unit (Elf { hitPoints = 200; attackPower = elfHitPower; })
     lines
     |> Seq.map (fun (row: string) ->
         row.Trim()
@@ -262,12 +288,31 @@ let parseWorld lines =
     |> Array.ofSeq
 
 let part1 () =    
-    let input = System.IO.File.ReadAllLines "c:\\dev\\aoc2018\\input\\15.txt"
-//    let input = @"#######
-//#G..#E#
-//#E#E.E#
-//#G.##.#
-//#...#E#
-//#...E.#
-//#######"
-    run (parseWorld (input))
+    //let input = System.IO.File.ReadAllLines "c:\\dev\\aoc2018\\input\\15.txt"
+    let input = @"#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######"
+    run (parseWorld (input.Split(Environment.NewLine)) 15)
+
+let part2 () =
+    let sw = Stopwatch.StartNew()
+    let input = @"#######
+#.G...#
+#...EG#
+#.#.#G#
+#..G#E#
+#.....#
+#######"
+    //let input = System.IO.File.ReadAllLines "c:\\dev\\aoc2018\\input\\15.txt"
+    let candidates = Seq.initInfinite id |> Seq.skip 4
+    let r =
+        candidates
+        |> Seq.map (fun p -> p, parseWorld (input.Split(Environment.NewLine)) p |> run)
+        |> Seq.pick (fun (p,r) -> r |> Option.map (fun x -> p,x) )
+        |> snd
+    printfn "%A" sw.Elapsed
+    r
--- aoc2018/Program.fs
+++ aoc2018/Program.fs
@@ -4,7 +4,7 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    DayFifteen.part1 ()
+    DayFifteen.part2 ()
         |> printfn "%A"
     Console.ReadLine () |> ignore
     0 // return an integer exit code
