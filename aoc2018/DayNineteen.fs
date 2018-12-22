756f51540223810589d8f4a52c39633f3d3331cd
duncanbrown <duncanphilipbrown@hotmail.com>
2018-12-22 13:34:13 +0000

    nineteen b

--- aoc2018/DayNineteen.fs
+++ aoc2018/DayNineteen.fs
@@ -6,7 +6,7 @@ open System
 
 let runProgram (program: Instruction[]) ipRegister =
     let registers = Array.zeroCreate 6
-    //registers.[0] <- 1L
+    registers.[0] <- 1L
     //registers.[1] <- reg1Val
     //registers.[2] <- 10551424L
     //registers.[3] <- 2L
@@ -15,7 +15,7 @@ let runProgram (program: Instruction[]) ipRegister =
 
 
     let rec loop i previous =
-        let previous' = previous //((i, Array.copy registers) :: previous)
+        let previous' = ((i, Array.copy registers) :: previous)
 
         let inline normalLoop () = 
             let instruction = program.[i]
@@ -24,36 +24,36 @@ let runProgram (program: Instruction[]) ipRegister =
             let i' = int ( registers.[ipRegister])
             if (i'+1) >= program.Length then registers.[0]
             else loop (i'+1) previous'
-        printfn "%i %A" i registers
-        normalLoop ()
-        //let p =
-        //    previous
-        //    |> List.tryFind
-        //        (fun (i',r) ->
-        //            i' = i &&
-        //            (Array.zip r registers)
-        //            |> Array.mapi (fun j (p, n) -> j,p,n)
-        //            |> Array.filter (fun (j,p,n) -> p <> n)
-        //            |> Array.length = 1)
-        //if p.IsSome
-        //then
-        //    //printfn "%A" p
-        //    let (j, p, n) = 
-        //        p.Value
-        //        |> snd
-        //        |> Array.mapi (fun j p -> j,p,registers.[j])
-        //        |> Array.find (fun (j,p,n) -> p <> n)
-        //    if false// j = 5 && i = 3 && registers.[5] < registers.[2] && registers.[1] < registers.[2]
-        //    then
-        //        printfn "%i %A" i registers
-        //        registers.[5] <- registers.[2]
-        //        printfn "%A" registers
-        //        loop i [i, Array.copy registers]//((i, Array.copy registers) :: previous)
-        //    else
-        //        normalLoop ()
-        //        //raise (Exception())
-        //else
-        //    normalLoop ()
+        //printfn "%i %A" i registers
+        //normalLoop ()
+        let p =
+            previous
+            |> List.tryFind
+                (fun (i',r) ->
+                    i' = i &&
+                    (Array.zip r registers)
+                    |> Array.mapi (fun j (p, n) -> j,p,n)
+                    |> Array.filter (fun (j,p,n) -> p <> n)
+                    |> Array.length = 1)
+        if p.IsSome
+        then
+            //printfn "%A" p
+            let (j, p, n) = 
+                p.Value
+                |> snd
+                |> Array.mapi (fun j p -> j,p,registers.[j])
+                |> Array.find (fun (j,p,n) -> p <> n)
+            if j = 5 && i = 3 // && registers.[5] < registers.[2] / registers.[1]
+            then
+                //printfn "%i %A" i registers
+                registers.[5] <- if registers.[5] > registers.[2] / registers.[1] then registers.[2] else registers.[2] / registers.[1]
+                //printfn "%A" registers
+                loop i [i, Array.copy registers]//((i, Array.copy registers) :: previous)
+            else
+                normalLoop ()
+                //raise (Exception())
+        else
+            normalLoop ()
             
     loop 0 []
 
