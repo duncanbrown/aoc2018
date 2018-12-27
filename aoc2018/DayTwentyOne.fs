55726781fd1f526c74120ad60173618dc1d5f639
duncanbrown <duncanphilipbrown@hotmail.com>
2018-12-27 19:36:28 +0000

    twentyone b

--- aoc2018/DayTwentyOne.fs
+++ aoc2018/DayTwentyOne.fs
@@ -3,30 +3,31 @@
 open FParsec
 open System
 open System.Diagnostics
+open FSharp.Collections.ParallelSeq
 
 let runProgram (program: DaySixteen.Instruction[]) ip =
     let registers = Array.zeroCreate 6
-    registers.[0] <- 1797184L
-    let rec loop i n =
-        if i = 28 then Debugger.Break()
-        let instruction = program.[i]
-        //printf "%A %A" instruction registers
-
-        registers.[ip] <- (int64 i)
-        DaySixteen.execute registers instruction
-        let i' = int ( registers.[ip])
-        //printfn " %A"  registers
-        if (i'+1) >= program.Length then n
-        else loop (i'+1) (n+1)
-    loop 0 ip
+    let rec loop i ones =
+        let inline doLoop ones' =
+            let instruction = program.[i]
+            registers.[ip] <- (int64 i)
+            DaySixteen.execute registers instruction
+            let i' = int ( registers.[ip])
+            if (i'+1) >= program.Length then raise (Exception())
+            else loop (i'+1) ones'
+        if i = 28
+        then
+            let valueOf1 = registers.[1]
+            if ones |> PSeq.exists (fun o -> o = valueOf1)
+            then List.head ones
+            else doLoop (valueOf1 :: ones)
+        else doLoop ones
+                
+    loop 0 List.empty
 
 let part1 () =
     let input = System.IO.File.ReadAllText "c:\\dev\\aoc2018\\input\\21.txt"
     match run DayNineteen.inputParser input with
     | Success ((ip, program),_,_) ->
         runProgram (Array.ofList program) ip
-        //Seq.initInfinite id
-        //|> Seq.map (runProgram (Array.ofList program) ip)
-        //|> Seq.pick id
     | Failure (err,_, _) -> raise(new Exception(err))
-
